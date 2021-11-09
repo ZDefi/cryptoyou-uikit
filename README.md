f429569d02a9ac94a5d06824a73f89b22dee3815
chainkf <chainkf@gmail.com>
2021-11-10 06:29:08 +0800

    init cryptoyou uikit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Cryptoyou UIkit
+
+This project is built with Typescript and export all the relevant types.
+
---
+++ dist/components/DropdownMenu/types.d.ts
@@ -0,0 +1,36 @@
+import React from "react";
+import { Colors } from "../../theme";
+import { BoxProps } from "../Box";
+export interface DropdownMenuProps extends BoxProps {
+    items?: DropdownMenuItems[];
+    activeItem?: string;
+    isBottomNav?: boolean;
+    openMenuTimeout?: number;
+    showItemsOnMobile?: boolean;
+    index?: number;
+    setMenuOpenByIndex?: React.Dispatch<React.SetStateAction<Record<number, boolean>>>;
+}
+export interface StyledDropdownMenuItemProps extends React.ComponentPropsWithoutRef<"button"> {
+    disabled?: boolean;
+    isActive?: boolean;
+}
+export declare enum DropdownMenuItemType {
+    INTERNAL_LINK = 0,
+    EXTERNAL_LINK = 1,
+    BUTTON = 2,
+    DIVIDER = 3
+}
+export interface LinkStatus {
+    text: string;
+    color: keyof Colors;
+}
+export interface DropdownMenuItems {
+    label?: string | React.ReactNode;
+    href?: string;
+    onClick?: () => void;
+    type?: DropdownMenuItemType;
+    status?: LinkStatus;
+    disabled?: boolean;
+    iconName?: string;
+    isMobileOnly?: boolean;
+}
